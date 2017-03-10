a44f825238e525b64d42815ce2295f6d2cf498e3
ThePuzzlers <damian@thepuzzlers.co>
2017-03-10 22:45:58 +1300

    basic lightbox

---
+++ js/images.js
@@ -0,0 +1,16 @@
+var thumbnails = [
+  {
+    tag : 'Hay Bales',
+    alt : 'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
+    src : 'photos/thumbnails/01.jpg'
+  },
+  {
+    tag : 'Lake',
+    alt : 'The lake was so calm today. We had a great view of the snow on the mountains from here.',
+    src : 'photos/thumbnails/02.jpg'
+  }
+];
+
+
+
+$('a[data-rel^=lightcase]').lightcase();
---
+++ js/photo-gallery.js
@@ -0,0 +1,8 @@
+// create an object which holds all images
+
+
+
+
+// put the images into different divs
+
+function ()
