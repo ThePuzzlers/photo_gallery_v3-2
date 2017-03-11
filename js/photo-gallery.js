02d5942cb7b0b53dbd3a718ef0f70da7e5dd78e1
ThePuzzlers <damian@thepuzzlers.co>
2017-03-11 18:44:32 +1300

    documentation of the code

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,25 +1,3 @@
-
-
-var imgString = '';
-var imgSrc;
-var thumbSrc;
-var alt;
-
-
-function print( message ) {
-  var outputDiv = document.getElementById('gallery');
-  outputDiv.innerHTML = message;
-}
-
-
-function imageListing(imgSrc, thumbSrc, alt) {
-
-  imgString += '<li><a href="' + imgSrc + '" data-rel="lightcase:myCollection"><img src="' + thumbSrc + '" alt="' + alt + '"></a></li>'
-
-  return imgString;
-}
-
-
 // what's always equal?
 // <li><a href="" data-rel="lightcase:myCollection"><img src="" alt=""></a></li>
 // what's changing?
@@ -30,19 +8,37 @@ function imageListing(imgSrc, thumbSrc, alt) {
 // Create a string with the informattions
 
 
+var imgString = ''; // to hold the final string
+var imgSrc;
+var thumbSrc;
+var alt;
 
+
+// to print out in DIV
+function print( message ) {
+  var outputDiv = document.getElementById('gallery');
+  outputDiv.innerHTML = message;
+}
+
+// creates a string with the certain values of the images
+function imageListing(imgSrc, thumbSrc, alt) {
+  imgString += '<li><a href="' + imgSrc + '" data-rel="lightcase:myCollection"><img src="' + thumbSrc + '" alt="' + alt + '"></a></li>'
+  return imgString;
+}
+
+
+
+// loops through the array and gets the data from the objects
 for (i = 0; i < images.length; i++) {
 
   imgSrc = images[i].imgSrc;
   thumbSrc = images[i].thumbSrc;
   alt = images[i].alt;
 
-  imageListing(imgSrc, thumbSrc, alt);
-
+  imageListing(imgSrc, thumbSrc, alt); // send it to the function so the string can get created
 }
 
-
-
+// print out the final string
 print(imgString);
 
 
