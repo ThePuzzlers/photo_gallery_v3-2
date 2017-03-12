7e57ef4ebc7ddbe36039a68cdfce0ffca723c31a
ThePuzzlers <damian@thepuzzlers.co>
2017-03-12 14:58:32 +1300

    wokring search for images in object

--- js/search.js
+++ js/search.js
@@ -2,7 +2,6 @@
 // the search schould listen to the form and respond on user input
 // afterwards the data should be submitted in real time
 
-var searchResult = [];
 
 
 function print( message ) {
@@ -13,31 +12,28 @@ function print( message ) {
 
 $( "#user-search" ).keydown(function( event ) {
 
+      var searchResult = [];
+      var matchingImgTag = [];
 
       if ((event.keyCode >= 48 && event.keyCode <= 90) || (event.keyCode >= 96 && event.keyCode <= 111) || (event.keyCode >= 186 && event.keyCode <= 222)) {
           var letter = event.key;
-
           searchResult.push(letter);
-
       }
 
       else if (event.keyCode == 8) {
           searchResult.pop();
-
       }
 
-      var searchResultConcat = searchResult.join('');
-      console.log(searchResultConcat);
+      for(i = 0; i < images.length; i++) {
 
-      // console.log(event);
+          var imagesTag = images[i].tag;
 
+          if(imagesTag.charAt(0) === searchResult[0] ) {
+            matchingImgTag.push(imagesTag);
+        }
+      }
 
-      // if (key === 'Backspace') {
-      //   console.log('Backspace');
-      // }
-    // searchResult += letter;
-
-    // console.log(letter);
+      console.log(matchingImgTag);
 
 
 });
