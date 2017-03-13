1e26f77156658185c49f95c55442eb3ecaa5f53b
ThePuzzlers <damian@thepuzzlers.co>
2017-03-13 13:14:09 +1300

    getting the images into a filtered array

--- js/search.js
+++ js/search.js
@@ -9,31 +9,73 @@ function print( message ) {
   outputDiv.innerHTML = message;
 }
 
+var searchResult = [];
+var matchingImgTag = [];
+var allImages = [];
+var filteredImages = [];
+var charCheck;
+
+
+for(i = 0; i < images.length; i++) {
+
+
+    var imagesTag = images[i].tag;
+    allImages.push(imagesTag.toLowerCase());
+}
 
 $( "#user-search" ).keydown(function( event ) {
 
-      var searchResult = [];
-      var matchingImgTag = [];
+
+
 
       if ((event.keyCode >= 48 && event.keyCode <= 90) || (event.keyCode >= 96 && event.keyCode <= 111) || (event.keyCode >= 186 && event.keyCode <= 222)) {
           var letter = event.key;
-          searchResult.push(letter);
+          searchResult.push(letter.toLowerCase());
       }
 
       else if (event.keyCode == 8) {
           searchResult.pop();
       }
 
-      for(i = 0; i < images.length; i++) {
+      if (searchResult.length == 1) {
 
-          var imagesTag = images[i].tag;
 
-          if(imagesTag.charAt(0) === searchResult[0] ) {
-            matchingImgTag.push(imagesTag);
+      for(i = 0; i < allImages.length; i++) {
+
+        if(allImages[i].charAt(0) === searchResult[0] && filteredImages.indexOf(searchResult) == -1) {
+        filteredImages.push(allImages[i]);
         }
       }
+    }
 
-      console.log(matchingImgTag);
+
+    // charCheck = searchResult.length;
+    //
+    // for(i = 0; i < filteredImages.length; i++) {
+    //
+    //   if(filteredImages[i].charAt(charCheck) != searchResult[charCheck]) {
+    //   filteredImages.pop(filteredImages[i]);
+    //   }
+    // }
+
+
+    //   for(i = 0; i < allImages.length; i++) {
+    //     if(allImages[i].charAt(charCheck) === searchResult[charCheck]) {
+    //     filteredImages.push(allImages[i]);
+    // }
+
+    //     else if(allImages[i].charAt(charCheck) != searchResult[charCheck]) {
+    //     filteredImages.pop(allImages[i]);
+    // }
+
+
+    console.log(filteredImages);
+
+
+      var searchResultConcat = searchResult.join('');
+     console.log(searchResultConcat);
+
+     console.log(charCheck);
 
 
 });
