7439291246ef803d0416beb82afb1727dadc518e
ThePuzzlers <damian@thepuzzlers.co>
2017-03-16 18:50:58 +1300

    WORKING caprion search
    used the javascript search function and smarter loops to get to the results faster

--- js/search.js
+++ js/search.js
@@ -16,6 +16,9 @@ var charCheck;
 var deletingImages = [];
 var loopCount = 0;
 var selectedImage;
+var imagesAlt;
+var altSearch;
+var searchPara;
 
 
 function charLoop (charCheck, userSearch) { // function to check if EVERY char matches
@@ -24,6 +27,7 @@ function charLoop (charCheck, userSearch) { // function to check if EVERY char m
 
   for (y = 0; y < userSearch; y++) { // loop through the chars of EACH char in EACH image \ One image at a time
     if(selectedImage.charAt(y) == searchResult[y]){
+      console.log(trueCount);
 
       // counter to check how many chars are matching
       trueCount = trueCount + 1;
@@ -32,12 +36,6 @@ function charLoop (charCheck, userSearch) { // function to check if EVERY char m
       }
     }
     return trueCount;
-}
-
-// get all images out of the 'database' before the start
-for(i = 0; i < images.length; i++) {
-    var imagesTag = images[i].tag;
-    allImages.push(imagesTag.toLowerCase());
 }
 
 
@@ -60,13 +58,25 @@ $( "#user-search" ).keydown(function( event ) { // function to check the user in
     $( "#gallery" ).empty(); // empty the id as soon as the user types in a letter | get rid of all displayed images
 
 
-    for(i = 0; i < allImages.length; i++) { // loop through the images
-      if(allImages[i].charAt(0) === searchResult[0]) {  // check if the first char matchs with the first of user input
-        if (filteredImages.indexOf(allImages[i]) == -1) { // check if the matching image is already in the filterd array
-            filteredImages.push(allImages[i]); // if not push the image
-        }
-      }
-    }
+    for(i = 0; i < images.length; i++) { // loop through the images
+      searchPara = searchResult.join("");
+
+      imagesAlt = images[i].alt.toLowerCase();
+
+
+      altSearch = imagesAlt.search(searchPara);
+
+
+      // console.log(altSearch + 'HEEEEELLLOOO');
+      if(altSearch >= 0) {  // check if the first char matchs with the first of user input
+        imgSrc = images[i].imgSrc;
+        thumbSrc = images[i].thumbSrc;
+        alt = images[i].alt;
+
+        imageListingSearch(imgSrc, thumbSrc, alt);
+        print(imgStringSearch); // get the data and print them out
+
+    } }
   } else if (searchResult.length == 0) { // if there is no user input
       for(i = -1; i <= filteredImages.length; i++) { // get rid of all images
         filteredImages.pop();
@@ -81,48 +91,10 @@ $( "#user-search" ).keydown(function( event ) { // function to check the user in
 
     ///////// All the code underneath works with this selection and updates it
 
-
-console.log(filteredImages);
-
-
-    charCheck = searchResult.length -1; // decrease the searchResult by one so we will start on 0 instead of 1
-    var userSearch = searchResult.length;
-
-
-        for(i = 0; i < filteredImages.length; i++) { // loop through the filtered images
-              selectedImage = filteredImages[i]; // store the active image
-              trueCount = charLoop(charCheck, userSearch); // use the function to check if EVERY char matches
-
-                if (trueCount === userSearch) { // do this if EACH char == to true before
-                  for (z = 0; z < images.length; z++) { // loop through the images to get the different data
-                    var imagesTagLowerCase = images[z].tag;
-                        imagesTagLowerCase = imagesTagLowerCase.toLowerCase();
-                        if (selectedImage === imagesTagLowerCase) { // compare the images and if they are equal go further
-
-
-                            imgSrc = images[z].imgSrc;
-                            thumbSrc = images[z].thumbSrc;
-                            alt = images[z].alt;
-
-                            imageListingSearch(imgSrc, thumbSrc, alt);
-                            print(imgStringSearch); // get the data and print them out
-                          }
-
-                        }
-
-
-                } else {
-                  // filteredImages.splice(filteredImages[i], 1)
-                  console.log(filteredImages[i] + 'FALSE');
-                }
-        }
-
-
-    imgStringSearch = ''; //reset the string so each item will just displayed once
-
     // for the Gallery Plugin - do not touch! Needs to get loaded last - inside the function for the search field
     $('a[data-rel^=lightcase]').lightcase();
-
+    filteredImages = [];
+    imgStringSearch = [];
 });
 
 
